2b48ae616a465abb9a52523e36bf25d4b4ba5aa7
MichelK <michel.kadilar@etu.univ-cotedazur.fr>
2024-10-19 19:28:56 +0200

    draft of one task oomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeMenuComponent} from './home-menu/home-menu.component';
+import {TaskComponent} from './list-tasks/task/task.component';
 
 export const routes: Routes = [
   {
-    path: '', component: HomeMenuComponent
+    path: '', component: TaskComponent
   }
 ];
