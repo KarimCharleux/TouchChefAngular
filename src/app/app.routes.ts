778b3a4e3fd38a78e027930277898407f534c0d5
MichelK <michel.kadilar@etu.univ-cotedazur.fr>
2024-10-19 15:54:19 +0200

    first draft of a fixed header with optional displays

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import {HomeMenuComponent} from './home-menu/home-menu.component';
+import {HeaderTouchchefComponent} from './header-touchchef/header-touchchef.component';
 
 export const routes: Routes = [
   {
